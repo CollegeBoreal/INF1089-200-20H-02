ce183dc117ee1459af3ba34e081f90d87a157521
setrar <setrar@valiha.com>
2020-02-27 17:14:57 -0500

    Update backup.ps1

--- 4.SSMS-PS/300098957/backup.ps1
+++ 4.SSMS-PS/300098957/backup.ps1
@@ -1,3 +1,1 @@
-RESTORE FILELISTONLY FROM DISK = 'C:\data\wwi.bak'
-RESTORE DATABASE WideWorldImporters FROM DISK = 'C:\data\wwi.bak' WITH MOVE 'WWI_Primary' TO 'C:\data\WideWorldImporters.mdf', MOVE 'WWI_UserData' TO 'C:\data\WideWorldImporters_userdata.ndf', MOVE 'WWI_Log' TO 'C:\data\WideWorldImporters.ldf', MOVE 'WWI_InMemory_Data_1' TO 'C:\data\WideWorldImporters_InMemory_Data_1'
 BACKUP DATABASE [WideWorldImporters] TO DISK = 'C:\data\wwi_2.bak' WITH NOFORMAT, NOINIT, NAME = 'WideWorldImporters-full', SKIP, NOREWIND, NOUNLOAD, STATS = 10
