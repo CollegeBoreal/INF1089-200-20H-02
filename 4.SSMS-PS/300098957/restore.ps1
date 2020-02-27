4c28a4b99d823093d3ace67857e32fa28c2d683a
setrar <setrar@valiha.com>
2020-02-27 17:11:43 -0500

    .

--- 4.SSMS-PS/300098957/restore.ps1
+++ 4.SSMS-PS/300098957/restore.ps1
@@ -1,4 +1,4 @@
 curl -OutFile "backup\wwi.bak" `
  "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak"
 docker exec --interactive --tty some-mssql `
-    sqlcmd -U sa -P "Password123" ` -i C:\DATA\restore.sql
+    sqlcmd -U sa -P "Password123" ` -i C:\DATA\scripts\restore.sql
