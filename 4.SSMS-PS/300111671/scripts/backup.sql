047e6d69a3e78ede6c13962b55bf0c784110315a
AbbasSadissou <aladeabbas@gmail.com>
2020-03-06 19:25:12 -0500

    d

--- 4.SSMS-PS/300111671/scripts/backup.sql
+++
@@ -1,2 +0,0 @@
-BACKUP DATABASE [WideWorldImporters] TO DISK = 'C:\data\backup\wwi_2.bak' WITH NOFORMAT, NOINIT, NAME = 'WideWorldImporters-full', SKIP, NOREWIND, NOUNLOAD, STATS = 10
-
--- 4.SSMS-PS/300111671/scripts/restore.sql
+++
@@ -1,9 +0,0 @@
-RESTORE FILELISTONLY FROM DISK = 'C:\data\backup\wwi.bak'
-RESTORE DATABASE WideWorldImporters FROM DISK = 'C:\data\backup\wwi.bak' WITH MOVE 'WWI_Primary' TO 'C:\data\WideWorldImporters.mdf', MOVE 'WWI_UserData' TO 'C:\data\WideWorldImporters_userdata.ndf', MOVE 'WWI_Log' TO 'C:\data\WideWorldImporters.ldf', MOVE 'WWI_InMemory_Data_1' TO 'C:\data\WideWorldImporters_InMemory_Data_1'
-
-<<<<<<< HEAD
-=======
-
-RESTORE FILELISTONLY FROM DISK = 'C:\data\backup\wwi.bak'
-RESTORE DATABASE WideWorldImporters FROM DISK = 'C:\data\backup\wwi.bak' WITH MOVE 'WWI_Primary' TO 'C:\data\WideWorldImporters.mdf', MOVE 'WWI_UserData' TO 'C:\data\WideWorldImporters_userdata.ndf', MOVE 'WWI_Log' TO 'C:\data\WideWorldImporters.ldf', MOVE 'WWI_InMemory_Data_1' TO 'C:\data\WideWorldImporters_InMemory_Data_1'
->>>>>>> 69e6def1ecf68b3ff5525f17d1e975cb51e714e3
