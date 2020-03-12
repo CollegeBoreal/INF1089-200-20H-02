2db53a446627bacc0df3f68c3b3b53d9391873c0
halima fadde <43480203+halimabzn@users.noreply.github.com>
2020-03-12 14:42:41 -0400

    💫

--- 5.Python/300108234/b300108234.py
+++ 5.Python/300108234/b300108234.py
@@ -4,8 +4,9 @@ from itertools import count, takewhile
 
 def primers():
   def seive(numbers)
-  yield head 
-  yield from sieve(n for n numbers if n % head)
+      head = next(numbers)
+      yield head 
+      yield from sieve(n for n numbers if n % head)
   return sieve(count(2))
 
 #list(takewhile(lambda x: x < 40, primes() ))
