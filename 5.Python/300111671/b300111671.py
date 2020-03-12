fab3cc1a0716dc8735e588313ad04cf45e17cea7
AbbasSadissou <43480195+AbbasSadissou@users.noreply.github.com>
2020-03-12 14:47:25 -0400

    Update b300111671.py

--- 5.Python/300111671/b300111671.py
+++ 5.Python/300111671/b300111671.py
@@ -5,3 +5,9 @@ Created on Thu Mar 12 14:08:13 2020
 @author: LENOVO
 """
 
+
+def prime ():
+  def sieve (numbers)
+  yield head 
+  yield from sieve(n for n in numbers if n % head)
+  return sieve (count (2))
