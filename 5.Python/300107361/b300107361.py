fbf8bae4ad12e14708a252afd0ab2de361f565ea
Etienne Tochgaly <etiennetochgaly@gmail.com>
2020-03-12 15:01:18 -0400

    :b:

--- 5.Python/300107361/b300107361.py
+++ 5.Python/300107361/b300107361.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 14:56:16 2020
+
+@author: Tochgaly-K.J.Etienne
+"""
+
 from itertools import count, takewhile 
 
 def primes():
@@ -6,6 +13,7 @@ def primes():
         yield head
         yield from sieve(n for n in numbers if n % head)
     return sieve (count(2))
-  
-  
- list(takewhile(lambda x: x < 60, primes())) 
+
+
+
+list(takewhile(lambda x: x < 60, primes())) 
