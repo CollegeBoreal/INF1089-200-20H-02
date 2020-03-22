30eca62b497a723b0e438cdb3d9e29e3de42ff73
ordenrosae <ordenrosae@yahoo.fr>
2020-03-22 04:03:32 -0400

    Ador

--- 5.Python/300112017/b300112017.py
+++ 5.Python/300112017/b300112017.py
@@ -5,11 +5,14 @@ Created on Thu Mar 12 14:35:00 2020
 @author: Ador
 """
 
-from iterttools import count, takewhile
+from itertools import count, takewhile
 
 def primes():
     def sieve(numbers):
         head = next(numbers)
         yield head
-        yield from sieve(n for in numbers if n % head)
+        yield from sieve(n for n in numbers if n % head)
         return sieve(count(2))
+     
+   list(takewhile(lambda x: x < 30, primes()))
+[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37]
