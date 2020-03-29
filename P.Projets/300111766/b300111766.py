a6752bbee2cae71737acfd10f1434f7d65323032
cheikthiam <44683625+Cheikhthiam@users.noreply.github.com>
2020-03-28 20:59:00 -0700

    .

---
+++ P.Projets/300111766/b300111766.py
@@ -0,0 +1,22 @@
+                                             StructuresdedonnéesPython
+                                             
+                                            
+ déﬁnirlaliste: liste =[17, 38, 10, 25, 72],puiseffectuezlesactionssuivantes: 
+ 
+ – triezetafﬁchezlaliste;
+ 
+ – ajoutezl’élément12àlalisteetafﬁchezlaliste; 
+ 
+ – renversezetafﬁchezlaliste; 
+ 
+ – afﬁchezl’indicedel’élément17; 
+ 
+ – enlevezl’élément38etafﬁchezlaliste;
+ 
+ – afﬁchezlasous-listedu2eau3eélément; 
+ 
+ – afﬁchezlasous-listedudébutau2eélément; 
+ 
+ – afﬁchezlasous-listedu3eélémentàlaﬁndelaliste; 
+ 
+ – afﬁchezlasous-listecomplètedelaliste;
