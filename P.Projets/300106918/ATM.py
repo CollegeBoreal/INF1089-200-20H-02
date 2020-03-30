15e1584b115909f104e6aed4412a28c255087758
AEKchaouche <43480300+AEKchaouche@users.noreply.github.com>
2020-03-30 00:08:40 -0400

    Update ATM.py

--- P.Projets/300106918/ATM.py
+++ P.Projets/300106918/ATM.py
@@ -4,25 +4,7 @@ Created on Thu Mar 26 00:36:16 2020
 
 @author: acer
 """
-      ## Exemple instruction break
-
-Dans cet exemple, nous recherchons un nombre «88» dans la liste de nombres donnée. La condition est d'afficher tous les nombres jusqu'à ce que le nombre «88» soit trouvé et quand il est trouvé, terminer la boucle et ne pas afficher le reste des nombres.
-
-      # programme pour afficher tous les éléments avant le numéro 88
-
-for num in [11, 9, 88, 10, 90, 3, 19]:
-   print(num)
-   if(num==88):
-	   print("The number 88 is found")
-	   print("Terminating the loop")
-	   break
-Output:
-11
-9
-88
-The number 88 is found
-Terminating the loop
-
+     
     ## programme de gestion de l'ATM,
 
     #tout l'utilisateur a besoin d'un mot de passe pour entrer dans le programme,
