f6050c5284917bba7b1750926a5b812be895e6bb
didier300110500 <43076393+didier300110500@users.noreply.github.com>
2020-04-02 15:41:48 -0400

    Update 300110500.ps1

--- 2.Shell/300110500/300110500.ps1
+++ 2.Shell/300110500/300110500.ps1
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+echo "Type the year that you want to check(4 digits), then press [ENTER]:"
+read year
+
+if (( ("$year" % 400) == "0" )) || (( ("$year" % 4) == "0" )) && (( ("$year % 100") != "0" ))
+   then
+      echo "$year is a leap year"
+else
+     echo "This is not a leap year"
+fi 
