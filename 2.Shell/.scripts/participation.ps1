c6e24ba2749efc393726815535c953a14bf8277d
b300098957 <300098957@collegeboreal.ca>
2020-04-12 15:45:15 -0400

    .

--- 2.Shell/.scripts/participation.ps1
+++ 2.Shell/.scripts/participation.ps1
@@ -4,8 +4,8 @@
 
 $ETUDIANTS = 300104524, 300104541, 300105201, 300106918, 300107361, 300108234, 300110500, 300111671, 300111766, 300112017, 300112917, 300113775
 
-echo "$ETUDIANTS"
+#echo "$ETUDIANTS"
 
-#for ($i = 0; $i -le $ETUDIANTS.Count; $i = $i + 1) {
-#    $ETUDIANTS[$i]
-#}
+for ($i = 0; $i -le $ETUDIANTS.Count; $i = $i + 1) {
+    $ETUDIANTS[$i]
+}
