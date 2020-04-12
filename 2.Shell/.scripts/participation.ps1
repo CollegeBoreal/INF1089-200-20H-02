b3a62eb671ee2a218bf50fb5170f5f62df61aecd
b300098957 <300098957@collegeboreal.ca>
2020-04-12 15:42:31 -0400

    .

--- 2.Shell/.scripts/participation.ps1
+++ 2.Shell/.scripts/participation.ps1
@@ -1,6 +1,8 @@
 #. ..\..\scripts\students.ps1
 
-Import-Module ..\..\.scripts\students.ps1
+#Import-Module ..\..\.scripts\students.ps1
+
+$ETUDIANTS = 300104524, 300104541, 300105201, 300106918, 300107361, 300108234, 300110500, 300111671, 300111766, 300112017, 300112917, 300113775
 
 echo "$ETUDIANTS"
 
