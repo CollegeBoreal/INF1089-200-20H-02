e88f49a99d3d7ac4b494ebceed71f5cc5776473c
Brice <62551735+b300098957@users.noreply.github.com>
2020-04-12 15:33:11 -0400

    Update participation.ps1

--- 2.Shell/.scripts/participation.ps1
+++ 2.Shell/.scripts/participation.ps1
@@ -1,5 +1,7 @@
-. ..\..\scripts\students.ps1
+#. ..\..\scripts\students.ps1
 
-for ($i = 0; $i -le $ETUDIANTS.Count; $i = $i + 1) {
-    $ETUDIANTS[$i]
-}
+echo "TOTO"
+
+#for ($i = 0; $i -le $ETUDIANTS.Count; $i = $i + 1) {
+#    $ETUDIANTS[$i]
+#}
