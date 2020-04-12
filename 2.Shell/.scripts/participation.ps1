1f52fa845aeb60e7af981c2ecd079391ccd8ea51
b300098957 <300098957@collegeboreal.ca>
2020-04-12 17:44:51 -0400

    .

--- 2.Shell/.scripts/participation.ps1
+++ 2.Shell/.scripts/participation.ps1
@@ -4,11 +4,11 @@
 $here = Split-Path --path ../../.scripts/students.ps1 -Resolve -Parent $MyInvocation.MyCommand.Path
 #$here = Split-Path -Path ../../.scripts/students.ps1 -Resolve -Parent
 echo $here
-. "$here\students.ps1"
+#. "$here\students.ps1"
 
 # $ETUDIANTS = 300104524, 300104541, 300105201, 300106918, 300107361, 300108234, 300110500, 300111671, 300111766, 300112017, 300112917, 300113775
 
-echo "$ETUDIANTS"
+#echo "$ETUDIANTS"
 
 #for ($i = 0; $i -le $ETUDIANTS.Count; $i = $i + 1) {
 #    $ETUDIANTS[$i]
