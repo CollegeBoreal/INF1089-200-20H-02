d4f07f46150e4cf97828bd1b0ef7ea535b137863
Brice <62551735+b300098957@users.noreply.github.com>
2020-04-12 18:46:10 -0400

    Update participation.ps1

--- 2.Shell/.scripts/participation.ps1
+++ 2.Shell/.scripts/participation.ps1
@@ -1,12 +1,10 @@
 $here = Split-Path -Path ..\..\  -Resolve -NoQualifier
 echo $here
 echo "$here\.scripts"
-Get-ChildItem $here
-
 $there = Split-Path -Path "$here\.scripts\*" 
 echo $there
 
-#. "$here\.scripts\students.ps1"
+. "$there\students.ps1"
 
 #echo $ETUDIANTS
 
