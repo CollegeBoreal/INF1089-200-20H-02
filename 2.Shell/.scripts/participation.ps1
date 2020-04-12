075446feb766703e038db92e3d22a383ba3eede6
b300098957 <300098957@collegeboreal.ca>
2020-04-12 15:38:51 -0400

    .

--- 2.Shell/.scripts/participation.ps1
+++ 2.Shell/.scripts/participation.ps1
@@ -1,6 +1,8 @@
 #. ..\..\scripts\students.ps1
 
-echo "TOTO"
+Import-Module ../../.scripts/students.ps1
+
+echo "$ETUDIANTS"
 
 #for ($i = 0; $i -le $ETUDIANTS.Count; $i = $i + 1) {
 #    $ETUDIANTS[$i]
